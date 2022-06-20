6030d60070e4a9e649b79be52b596da304b918c2
legehwahn <legehwahn@gmail.com>
2022-06-20 20:28:25 +0200

    build: switch to webpack dev server

--- src/web/config/webpack.dev.js
+++ src/web/config/webpack.dev.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import webpack from 'webpack';
 import { merge } from 'webpack-merge';
 
@@ -6,11 +7,19 @@ import common from './webpack.common.js';
 export default merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
+	devServer: {
+		static: {
+			directory: path.resolve(process.cwd(), 'dist')
+		},
+		port: 3000,
+		open: true,
+		historyApiFallback: true
+	},
 	plugins: [
 		// Inject environment values
 		new webpack.EnvironmentPlugin({
 			WEB_SERVER_URL: 'http://localhost:3000'
-		}),
+		})
 	],
 	performance: {
 		hints: false
